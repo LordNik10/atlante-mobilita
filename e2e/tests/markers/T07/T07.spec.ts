4bf60c41210eb2dc967c31e043631329aa2b9938
LordNik10 <niccolonaso@yahoo.it>
2026-03-10 13:14:56 +0100

    fix: update BASE_URL usage in tests and fixtures for consistency

--- e2e/tests/markers/T07/T07.spec.ts
+++ e2e/tests/markers/T07/T07.spec.ts
@@ -1,11 +1,11 @@
 import { getFeatures } from "@/e2e/features";
-import { test } from "@/fixtures";
+import { BASE_URL, test } from "@/fixtures";
 import { createStepCounter } from "@/e2e/utils";
 import { T07Operations } from "./T07Operations";
 import { T07Checks } from "./T07Checks";
 
 test("User must be logged to create report", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+  await page.goto(BASE_URL);
   const stepCounter = createStepCounter();
   const { MapFeature } = getFeatures(page);
   const operations = new T07Operations(page);
--- fixtures.ts
+++ fixtures.ts
@@ -1,20 +1,21 @@
 import { Page, test as base } from "@playwright/test";
 import { getFeatures } from "./e2e/features";
 
-const URL = "http://localhost:3000/login";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+
+const URL = `${BASE_URL}/login`;
 const E2E_EMAIL = process.env.E2E_EMAIL;
 const E2E_PASSWORD = process.env.E2E_PASSWORD;
 
 if (!E2E_EMAIL || !E2E_PASSWORD) {
   throw new Error(
-    "Missing E2E_EMAIL and/or E2E_PASSWORD environment variables for Playwright E2E tests."
+    "Missing E2E_EMAIL and/or E2E_PASSWORD environment variables for Playwright E2E tests.",
   );
 }
 
 type MyFixtures = {
-    authPage: Page;
-  };
-  
+  authPage: Page;
+};
 
 export const test = base.extend<MyFixtures>({
   authPage: async ({ page }, runWithPage) => {
